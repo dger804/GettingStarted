5008d9500e30b4a4b64d3c8de6efb61f8757a789
dger804 <diego804@gmail.com>
2020-07-04 12:08:26 -0500

    Initial commit

---
+++ program.js
@@ -0,0 +1,11 @@
+//var lunch = 'pizza'; console.log(4);
+//console.log(lunch);
+var totalCost = 14.99;
+
+var sizeOfParty = process.argv[2];
+
+var averageCost = totalCost / sizeOfParty;
+
+console.log("$"+averageCost);
+//console.log(process.argv);
+//var sizeOfParty = process.title
