b351de400df6600ee1c65b6a4c07ca1616f299e7
dger804 <diego804@gmail.com>
2020-07-04 12:59:47 -0500

    Functions
    On JavaScript

--- variables.js
+++ variables.js
@@ -4,6 +4,13 @@ var z = "3";
 var type = typeof 5;
 var result = x > y;
 
+var writeGreeting = function(name){
+	var message = "hello, " + name;
+	console.log(message);
+}
+writeGreeting("diego");
+writeGreeting("Alejandro");
+
 console.log(x);
 console.log(y);
 console.log(typeof y);
@@ -12,4 +19,4 @@ console.log(type);
 console.log(typeof result);
 console.log(result);
 console.log(typeof console);
-console.log(console);
+//console.log(console);
