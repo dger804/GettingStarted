0d5eaf6969a5fade9fdbfcdc142f80d9dac896c9
dger804 <diego804@gmail.com>
2020-07-04 13:26:00 -0500

    Functions
    Parameters

--- functions.js
+++ functions.js
@@ -1,20 +1,13 @@
-var getDieRoll = function(){
+var getDieRoll = function(dieSize, var2, var3){
 	console.log("rolling a die");
-	return Math.ceil(6*Math.random())
+	return Math.ceil(dieSize*Math.random())
 };
 
-var firstDie = getDieRoll();
-
-
-//var value=6*Math.random(); //se multiplica por 6 para tener un valor mayor entre 0.0 y 1
-//var roll =Math.ceil(value); //redondea
-//console.log(roll);
-//console.log(Math.ceil(6*Math.random()));
-
+var firstDie = getDieRoll(10);
 
 console.log(firstDie);
 
-var secondDie = getDieRoll();
+var secondDie = getDieRoll(3);
 console.log(secondDie);
 console.log(firstDie + secondDie);
 
