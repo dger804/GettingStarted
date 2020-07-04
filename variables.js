770843caff6874703dc851c2418db6e5e1351a2d
dger804 <diego804@gmail.com>
2020-07-04 12:39:28 -0500

    Update variables.js
    Types

--- variables.js
+++ variables.js
@@ -1,3 +1,15 @@
-var x = 3;
+var x = 3,
+	y = 2; 
+var z = "3";
+var type = typeof 5;
+var result = x > y;
 
 console.log(x);
+console.log(y);
+console.log(typeof y);
+console.log(typeof z);
+console.log(type);
+console.log(typeof result);
+console.log(result);
+console.log(typeof console);
+console.log(console);
