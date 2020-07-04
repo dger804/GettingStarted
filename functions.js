dffcee9b4e6b2096be8ce5a3f1612eecd8289f7c
dger804 <diego804@gmail.com>
2020-07-04 13:15:20 -0500

    Functions

---
+++ functions.js
@@ -0,0 +1,13 @@
+var firstDie = Math.ceil(6*Math.random());
+var secondDie = Math.ceil(6*Math.random());
+
+//var value=6*Math.random(); //se multiplica por 6 para tener un valor mayor entre 0.0 y 1
+//var roll =Math.ceil(value); //redondea
+//console.log(roll);
+
+
+//console.log(Math.ceil(6*Math.random()));
+console.log(firstDie);
+console.log(secondDie);
+console.log(firstDie + secondDie);
+
