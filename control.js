dd335577817778b8d691ca47d9d677f4d88bddbc
dger804 <diego804@gmail.com>
2020-07-07 22:14:41 -0500

    While Do While

--- control.js
+++ control.js
@@ -4,17 +4,24 @@ var getDieRoll = function(dieSize){
 	return result;
 };
 var roll = getDieRoll(6);
-if(roll >= 3 ){ // roll >= 3 && roll <= 5
-	if(roll <= 5){
-		console.log("Great roll!");
-		
-	}
-	
-}else if(roll == 1){
-	
-	console.log("That was a terrible");
-}else {
-	
+
+while(roll < 4){
+	console.log(roll);
+	roll = getDieRoll(6); 	
+}
+
+do{	
+	roll = getDieRoll(6);
+	console.log(roll);
+}while(roll > 4)
+
+if(roll >= 3 && roll <= 5){ // roll >= 3 && roll <= 5
+	console.log("Great roll!");
+}
+else if(roll == 1){	
+	console.log("Terrible roll");
+}
+else{	
 	console.log("That was an ok roll");
 }
 console.log("You rolled a "+ roll);
