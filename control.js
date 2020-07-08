a879d335d76fea48ef976fc2d3a4a07c9f6ef677
dger804 <diego804@gmail.com>
2020-07-08 00:16:28 -0500

    Arrays Revisited

--- control.js
+++ control.js
@@ -3,22 +3,20 @@ var getDieRoll = function(dieSize){
 	var result = Math.ceil(dieSize*Math.random());
 	return result;
 };
+
+var things = [1,2,3,"pizza"];
+
+//console.log(things[3]);
+for(var i = 0; i < process.argv.length; i ++ ){ //i< things.length
+	console.log(process.argv[i]);	
+}
+
 var roll = getDieRoll(6);
 
 for(var i = 0; i < 5; i += 1){
 	console.log(roll);
 	roll = getDieRoll(6);
 }
-
-while(roll < 4){
-	console.log(roll);
-	roll = getDieRoll(6); 	
-}
-
-do{	
-	roll = getDieRoll(6);
-	console.log(roll);
-}while(roll > 4)
 
 if(roll >= 3 && roll <= 5){ // roll >= 3 && roll <= 5
 	console.log("Great roll!");
