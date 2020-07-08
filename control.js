5db5a8ae8af20b2a995e362f24ca4dcddffc2413
dger804 <diego804@gmail.com>
2020-07-07 21:50:19 -0500

    If Variations

--- control.js
+++ control.js
@@ -4,10 +4,18 @@ var getDieRoll = function(dieSize){
 	return result;
 };
 var roll = getDieRoll(6);
-if(roll>=2 && roll <=3){
+if(roll >= 3 ){ // roll >= 3 && roll <= 5
+	if(roll <= 5){
+		console.log("Great roll!");
+		
+	}
 	
-	console.log("Great roll!")
+}else if(roll == 1){
+	
+	console.log("That was a terrible");
+}else {
+	
+	console.log("That was an ok roll");
 }
-
-
 console.log("You rolled a "+ roll);
+
