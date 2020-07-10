04920c1db5439f9eb81dccaab67d05913bbcddc1
dger804 <diego804@gmail.com>
2020-07-09 21:06:23 -0500

    Splitting Up

--- program.js
+++ program.js
@@ -1,19 +1,8 @@
-var getDieRoll = function(dieSize){
-	var result = Math.ceil(dieSize*Math.random());
-	return result;
-};
+var dice = require("./dice");
+var die = dice.die;
 
-var die = {
-	size: 6,
-	totalRolls: 0,
-	roll: function(dieSize){
-		var result = Math.ceil(this.size*Math.random());
-		this.totalRolls += 1;
-		return result;
-	}
-};
 die.size = 10;
-console.log(die);
+
 console.log(die.roll());
 console.log(die.roll());
 console.log(die.roll());
