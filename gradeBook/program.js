65d2d5d99c34b82c41db723e38b3c64498a57c31
dger804 <diego804@gmail.com>
2020-07-09 22:31:38 -0500

    Web Grades

--- gradeBook/program.js
+++ gradeBook/program.js
@@ -7,5 +7,18 @@ app.get("/", function(req, res){
 	res.send("Hello, world!");
 });
 
+app.get("/grade", function(req, res){
+	
+	//"1,2,3".split(",") => [1,2,3];
+	var grades = req.query.grades.split(",");
+	for (var i = 0; i < grades.length; i+=1){
+		book.addGrade(parseInt(grades[i]));
+	}
+	var average = book.getAverage();
+	var letter = book.getLetterGrade();
+	
+	res.send("Your average is " + average + " grade " + letter);
+})
+
 app.listen(3000);
 console.log("Server ready...");
